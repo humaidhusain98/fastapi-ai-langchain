24f947ca2cc5e8ed63ccb832ff7d6f4f63c530c5
humaidhusain98 <66073877+humaidhusain98@users.noreply.github.com>
2025-02-11 15:30:26 +0300

    Update rag.py

--- routers/rag.py
+++ routers/rag.py
@@ -4,7 +4,13 @@ from pydantic import BaseModel
 
 class Body(BaseModel):
     inputFileName:str
-    outputFileName:str
+    outputFolderName:str
+
+class RagRetrieval(BaseModel):
+    query: str
+    vector_store_name: str
+    search_type: str
+    search_kwargs: object
 
 router = APIRouter(
     prefix="/rag"
@@ -13,7 +19,21 @@ router = APIRouter(
 @router.post("/generateEmbeddingsFromTextFile")
 async def generateEmbeddingsTxtFile(body:Body,response:Response):
     print(body)
-    generatedResponse = embeddings.generateEmbeddingsFromTextFile(body.inputFileName,body.outputFileName)
+    generatedResponse = embeddings.generateEmbeddingsFromTextFile(body.inputFileName,body.outputFolderName)
+    response.status_code=generatedResponse.httpCode
+    return generatedResponse
+
+@router.post("/retrieveRagDataFromVectorStore")
+async def retrieveRagData(body:RagRetrieval,response:Response):
+    print(body)
+    generatedResponse = embeddings.retrieveDataFromEmbeddings(body.query,body.vector_store_name,body.search_type,body.search_kwargs)
+    response.status_code=generatedResponse.httpCode
+    return generatedResponse
+
+@router.post("/generateGPT-4o-miniRAG")
+async def gpt4ominigeneration(body:RagRetrieval,response:Response):
+    print(body)
+    generatedResponse = embeddings.chatgptPlusRagRetriever(body.query,body.vector_store_name,body.search_type,body.search_kwargs)
     response.status_code=generatedResponse.httpCode
     return generatedResponse
 
